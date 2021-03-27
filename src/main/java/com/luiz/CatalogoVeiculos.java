d16ff0798f7e344dc5a799d977a8e26d86ce3b27
Wolxf2 <luizjirkowsky19@outlook.com>
2021-03-27 21:12:20 +0000

    Modificação CatalogoVeiculos MAVEN

--- src/main/java/com/luiz/CatalogoVeiculos.java
+++ src/main/java/com/luiz/CatalogoVeiculos.java
@@ -1,17 +1,56 @@
 package com.luiz;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import java.nio.file.Files;
+import java.io.Reader;
+import java.nio.file.Paths;
 
 public class CatalogoVeiculos {
     private List<Veiculo> veiculos;
+
+
+    private void carrega(String nome) {
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get("src/main/java/com/luiz/resource/" + nome));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader());
+        ) {
+                for (CSVRecord csv : csvParser) {
+                String placa = csv.get(0);
+                String marca = csv.get(1);
+                String modelo = csv.get(2);
+                int ano = Integer.parseInt(csv.get(3));
+                double valor = Double.parseDouble(csv.get(4));
+
+                if(nome.equals("VeiculoDePassageiros.csv")) {
+                    int nroPass = Integer.parseInt(csv.get(5));
+                    this.veiculos.add(new VeiculoPassageiros(placa, marca, modelo, ano, valor, nroPass));
+                }
+
+                if(nome.equals("VeiculoDePasseio.csv")) {
+                    double consumoKmLt = Double.parseDouble(csv.get(5));
+                    this.veiculos.add(new VeiculoPasseio(placa, marca, modelo, ano, valor, consumoKmLt));
+                }
+
+                if(nome.equals("VeiculoUtilitario.csv")) {
+                    int capCargaKg = Integer.parseInt(csv.get(5));
+                    int nroEixos = Integer.parseInt(csv.get(6));
+                    this.veiculos.add(new VeiculoUtilitario(placa, marca, modelo, ano, valor, capCargaKg, nroEixos));
+                }
+
+            }
+        }catch(Exception e) {
+            System.err.println("ERRO");
+        }
+    }
     
     public CatalogoVeiculos() {
         this.veiculos = new ArrayList<>();
-        veiculos.add(new VeiculoPasseio("BRA2E19", "Mitsubishi", "Lancer", 2020, 250150.5, 12.5));
-
-        veiculos.add(new VeiculoUtilitario("RIO2A18", "Iveco", "Daily", 2019, 75000.0, 500, 2));
-
-        veiculos.add(new VeiculoPassageiros("PBA2017", "Marcopolo", "Paradiso", 2017, 225749.5, 45));
+        carrega("VeiculoDePassageiros.csv");
+        carrega("VeiculoDePasseio.csv");
+        carrega("VeiculoUtilitario.csv");
     }
     
     public Veiculo consultaPorPlaca (String placa){
